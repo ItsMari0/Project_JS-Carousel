dc58c3ecd445b55b18e9664da03990fa231c4983
Eugene Mazurkevich <evgen.mazurkevich@gmail.com>
2022-02-11 16:19:40 +0300

    create a keyPress & swipes

--- js/main.js
+++ js/main.js
@@ -5,24 +5,37 @@
 <li class="slide">Slide 4</li>
 <li class="slide">Slide 5</li>
 </ul> */
+//swipe
+const container = document.querySelector('#carousel');
+const slides = container.querySelectorAll(".slide");
+const indicatorsContainer = container.querySelector('#indicators-container')
+const indicators = indicatorsContainer.querySelectorAll('.indicator')
+const pauseBtn = container.querySelector("#pause-btn");
+const prevBtn = container.querySelector("#prev-btn");
+const nextBtn = container.querySelector("#next-btn");
 
-const slides = document.querySelectorAll(".slide");
-const pauseBtn = document.querySelector("#pause-btn");
-const prevBtn = document.querySelector("#prev-btn");
-const nextBtn = document.querySelector("#next-btn");
+
 
 const SLIDES_COUNT = slides.length;
 const PAUSE = 'Pause';
 const PLAY = 'Play';
+const CODE_LEFT_ARROW = 'ArrowLeft';
+const CODE_RIGHT_ARROW = 'ArrowRight';
+const CODE_SPACE = 'Space';
+
 let currentSlide = 0;
 let isPlaying = true;
 let timerID = null;
 let interval = 2000;
+let swipeStartX = null;
+let swipeEndX = null;
 
 function goToNth(n) {
-    slides[currentSlide].className = "slide";
+    slides[currentSlide].classList.toggle('active');
+    indicators[currentSlide].classList.toggle('active');
     currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
-    slides[currentSlide].className = "slide active";
+    slides[currentSlide].classList.toggle('active');
+    indicators[currentSlide].classList.toggle('active');
 }
 
 function goToPrev() {
@@ -63,8 +76,44 @@ function next() {
     goToNext();
 }
 
+function indicate(e) {
+    const target = e.target;
+    if (target.classList.contains('indicator')) {
+        pause();
+    }
+    // console.log(target.getAttribute('data-slide-to')); // выведет строку, т.к значение атрибута это строка;
+    // we can change this construction:
+    // goToNth(Number(target.getAttribute('data-slide-to')));
+    // on this construction:
+    goToNth(Number(target.dataset.slideTo));
+    /* data- ==> dataset;
+        slide-to ==>slideTo;*/
+
+}
+
+function pressKey(e) {
+    if (e.code === CODE_LEFT_ARROW) prev();
+    if (e.code === CODE_RIGHT_ARROW) next();
+    if (e.code === CODE_SPACE) pausePlay();
+}
+
+function swipeStart(e) {
+    swipeStartX = e.changedTouches[0].pageX;
+}
+
+function swipeEnd(e) {
+    swipeEndX = e.changedTouches[0].pageX;
+    if (swipeStartX - swipeEndX < -100) prev();
+    if (swipeStartX - swipeEndX > 100) next();
+}
+
 pauseBtn.addEventListener("click", pausePlay);
 prevBtn.addEventListener("click", prev);
 nextBtn.addEventListener("click", next);
+indicatorsContainer.addEventListener('click', indicate);
+
+document.addEventListener('keydown', pressKey);
+container.addEventListener('touchstart', swipeStart);
+container.addEventListener('touchend', swipeEnd);
 
 timerID = setInterval(goToNext, interval);
